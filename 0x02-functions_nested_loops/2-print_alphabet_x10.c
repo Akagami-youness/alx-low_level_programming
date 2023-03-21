5f93c82640a670ff8c0fdffb70c35fa81757798b
youness <oua.youness19@gmail.com>
2023-03-21 16:42:16 -0700

    Funciotns

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
-* main - Entry point 
-* Return: Always 0
+* print_alphabet_x10 - Prints the alphabet 10 times.
+*
+* Return: Always 0 (Success)
 */
 void print_alphabet_x10(void)
 {
