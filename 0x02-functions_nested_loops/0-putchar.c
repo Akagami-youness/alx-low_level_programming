a35921475aebecfdbbf96d0ab7d48f54645086ad
youness <oua.youness19@gmail.com>
2023-03-21 16:19:52 -0700

    c_functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- * main - Entry point
- * Return: Always 0 
- */
+* main - Entry point
+* Desciption: prints _putchar to stdout
+* Return: Always 0
+*/
 int main(void)
 {
 _putchar('_');
